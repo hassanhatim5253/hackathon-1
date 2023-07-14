a2092e5200e8317147060f3a7e75fa6b604a1fd8
hassanhatim5253 <137111036+hassanhatim5253@users.noreply.github.com>
2023-07-15 04:48:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", this.window.scrollY >0);
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+// menu.onclick = () => {
+//     menu.classList.toggle ('bx-x');
+//     navmenu.classList.toggle ('open');
+// }
+
+
+let products;
+let getProducts = async () => {
+    let products = await fetch("https://dummyjson.com/products");
+    let response = await products.json();
+    // products = await response.products;
+    // console.log(response.products);
+    localStorage.setItem("products", JSON.stringify(response.products))
+    productsRendering(response.products);
+}
+
+
+
+let productsRendering = (data) => {
+   
+    let productsRow = document.getElementById("productsRow");
+
+    data.forEach(p => {
+        let product = document.createElement("div");
+        product.classList.add("col-3");
+        product.classList.add("my-2");
+        product.innerHTML = `
+        <div class="product border">
+                  <img src="${p.thumbnail}" alt="" class="w-100">
+                  <h5 class="ms-2"><a href="./productDetails.html?pid=${p.id-1}">${p.title}</a></h5>
+                  <div class="stars ms-2 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                  </div>
+                  <p class="ms-2">${p.rating}</p>
+                  <p class="ms-2 fw-bolder h5">$ ${p.price}</p>
+                  <button class="btn btn-primary m-2" onclick="addToCart(${p.id-1})">add to cart <i class="bi bi-cart-plus"></i></button>
+
+                </div>
+        `
+        // console.log(product);
+        productsRow.appendChild(product);
+    });
+}
+getProducts();
+
+let cart = [];
+let addToCart = (id) => {
+    event.preventDefault();
+    // alert("product add " + id);
+    let product = JSON.parse(localStorage.getItem("products"));
+    cart.push(product[id]);
+    console.log(product[id]);
+    renderCartItems();
+}
+
+let renderCartItems = () => {
+    document.getElementById("cartItems").innerHTML = "";
+    let cartItems = document.getElementById("cartItems");
+    cart.forEach(product => {
+        cartItems.innerHTML += `
+            <div class="d-flex justify-content-between">
+                <img src="${product.thumbnail}" class="w-25" alt="">
+                <h5 class=" mt-3">${product.title}</h5>
+                <p class=" mt-3">${product.price}</p>
+                <i class="bi bi-x mt-3"></i>
+             </div>
+        `
+    });
+    console.log(cart);
+}
